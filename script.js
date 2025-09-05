03593f3497d9faaeb2a2021f5d9202e9677a3b29
Dachi Darjania <dachidar19@gmail.com>
2025-09-05 15:20:22 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//#region searchbar open/close functions
+function openSearchField(){
+  const searchField = document.querySelector(".search-field");
+  searchField.style.display = 'unset';
+}
+
+function closeSearchField(){
+  const searchField = document.querySelector(".search-field");
+  searchField.style.display = "none";
+}
+//#endregion
+//#region registation form open/close functions
+function openRegistrationForm(){
+  const accountForm = document.querySelector(".account-form");
+  accountForm.style.display = "flex";
+}
+function closeRegistrationForm(){
+  const accountForm = document.querySelector(".account-form");
+  accountForm.style.display = "none";
+}
+//#endregion
+//#region cart open/close functions
+function openCart(){
+  const cart = document.querySelector(".cart");
+  const darkBG = document.querySelector(".header__dark-bg");
+  cart.style.display = "unset";
+  darkBG.style.display = "unset";
+}
+function closeCart(){
+  const cart = document.querySelector(".cart");
+  const darkBG = document.querySelector(".header__dark-bg");
+  cart.style.display = "none";
+  darkBG.style.display = "none";
+}
+//#endregion
+//#region burger menu open/close functions
+function openSidebar(){
+  const sidebar = document.querySelector(".header__mobile-ul");
+  sidebar.style.display = "flex";
+}
+function closeSidebar(){
+  const sidebar = document.querySelector(".header__mobile-ul");
+  sidebar.style.display = "none";
+}
+//#endregion
