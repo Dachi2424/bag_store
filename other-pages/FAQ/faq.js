3a542e1d470f5cfb9e07e7fd953e2cd498ff77ea
Dachi Darjania <dachidar19@gmail.com>
2025-09-09 16:26:41 +0400

    davamate

---
+++ other-pages/FAQ/faq.js
@@ -0,0 +1,10 @@
+function showQuestionField(){
+  const field = document.querySelector(".faq__search-container");
+  field.style.display = "unset";
+}
+function hideQuestionField(){
+  const field = document.querySelector(".faq__search-container");
+  field.style.display = "none";
+}
+
+
